d6b05c23e40b9711105a525fc0240db56e900108
Mateusz Jończyk <mat.jonczyk@o2.pl>
2014-09-07 20:19:36 +0200

    Renamed Notatki_nowe.html -> index.html

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 
 perl ../markdown/Markdown_1.0.1/Markdown.pl --html4tags < Notatki_nowe.md > Notatki_nowe_body.html
 
-cat start.html Notatki_nowe_body.html end.html > Notatki_nowe.html
+cat start.html Notatki_nowe_body.html end.html > index.html
